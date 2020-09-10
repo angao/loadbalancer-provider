60fdb56ca715ef2474a9105c16ebcadaddfdd042
Wen Gao <gaowen@caicloud.io>
2020-09-10 11:09:12 +0800

    update vendor to support kong proxy

--- vendor/github.com/caicloud/clientset/pkg/apis/resource/v1beta1/register.go
+++ vendor/github.com/caicloud/clientset/pkg/apis/resource/v1beta1/register.go
@@ -62,6 +62,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ExtendedResourceList{},
 		&NodeClaim{},
 		&NodeClaimList{},
+		&ClusterResourceScale{},
+		&ClusterResourceScaleList{},
 	)
 
 	// Add common types
